fd89434f1940b582564c9ddd16a864e2385f15ab
Juan Pablo Gonzales <juampi7237@gmail.com>
2020-12-09 20:47:00 -0400

    maximizar y cerrar el navegador

--- src/com/company/PracticaWebDriver.java
+++ src/com/company/PracticaWebDriver.java
@@ -10,9 +10,13 @@ public class PracticaWebDriver {
         //open the chrome browser
         System.setProperty("webdriver.chrome.driver","C://Users//juamp//Documents//selenium jars drivers/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        String urlBase = "https://letskodeit.teachable.com/p/practice";
+        driver.get(urlBase);
+        driver.manage().window().maximize();
+        driver.quit();
 
         //open the firefox browser
-        System.setProperty("webdriver.gecko.driver","C://Users//juamp//Documents//selenium jars drivers/drivers/geckodriver.exe");
-        WebDriver driverf = new FirefoxDriver();
+        //System.setProperty("webdriver.gecko.driver","C://Users//juamp//Documents//selenium jars drivers/drivers/geckodriver.exe");
+        //WebDriver driverf = new FirefoxDriver();
     }
 }
