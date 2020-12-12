34c695cc407523404b7f529c5bc9f2e039b9d25d
Juan Pablo Gonzales <juampi7237@gmail.com>
2020-12-12 15:59:42 -0400

    selectores de id xpath name linktex

--- src/com/company/PracticaWebDriver.java
+++ src/com/company/PracticaWebDriver.java
@@ -1,19 +1,30 @@
 package com.company;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class PracticaWebDriver {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //open the chrome browser
         System.setProperty("webdriver.chrome.driver","C://Users//juamp//Documents//selenium jars drivers/drivers/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        String urlBase = "https://letskodeit.teachable.com/p/practice";
+        String urlBase = "https://letskodeit.teachable.com";
         driver.get(urlBase);
         driver.manage().window().maximize();
-        driver.quit();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //driver.findElement(By.xpath("//a[@href='/sign_in']")).click();
+        //driver.findElement(By.id("user_email")).sendKeys("letskodeit@gmail.com");
+        driver.findElement(By.partialLinkText("Pract")).click();
+        driver.findElement(By.name("enter-name")).sendKeys("Juan Pablo");
+        Thread.sleep(3000);
+        driver.findElement(By.linkText("Login")).click();
+        driver.findElement(By.id("user_email")).sendKeys("juampi@gmail.com");
+        //driver.quit();
 
         //open the firefox browser
         //System.setProperty("webdriver.gecko.driver","C://Users//juamp//Documents//selenium jars drivers/drivers/geckodriver.exe");
